1627416f13fba2be95e0ad91db12d8ecc6b3b1e6
Guilherme Souza Gamal <91894486+GamalOficial@users.noreply.github.com>
2022-03-27 22:11:52 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Questao01 from './components/Questao01';
+import Questao02 from './components/Questao02';
+import Questao02b from './components/Questao02b';
+import Questao02c from './components/Questao02c';
+function App() {
+  return (
+    <div className="App">
+      <Questao01/>
+      <Questao02/>
+      <Questao02b/>
+      <Questao02c/>
+    </div>
+  );
+}
+
+export default App;
