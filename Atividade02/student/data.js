032a1cb4445877b90296b497c75653fb644e06e5
Guilherme Souza Gamal <91894486+GamalOficial@users.noreply.github.com>
2022-04-19 00:29:44 -0300

    Create data.js

---
+++ Atividade02/student/data.js
@@ -0,0 +1,10 @@
+const students = [
+  { id: 0, name: "Guilherme Gamal", course: "DD", ira: 6.7 },
+  { id: 1, name: "Joffensolis", course: "DD", ira: 5.2 },
+  { id: 2, name: "Carleixous", course: "DD", ira: 7.1 },
+  { id: 3, name: "JonasBrother", course: "DD", ira: 6.2 }
+];
+
+const PI = 3.14;
+
+export { students, PI };
