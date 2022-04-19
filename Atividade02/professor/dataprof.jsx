1d92ec3de569a11477817268696fe1f5bfd4aa12
Guilherme Souza Gamal <91894486+GamalOficial@users.noreply.github.com>
2022-04-19 00:32:45 -0300

    Create dataprof.jsx

---
+++ Atividade02/professor/dataprof.jsx
@@ -0,0 +1,18 @@
+const professors = [
+  { id: 0, name: "Joãozinho, o Brabo", university: "UFC", degree: "DD" },
+  {
+    id: 1,
+    name: "George, o Mamaciter",
+    university: "UFC",
+    degree: "Publicidade e Propaganda"
+  },
+  {
+    id: 2,
+    name: "Xúlia, a Kpoper",
+    university: "UFC",
+    degree: "Desenvolvimento de Software"
+  },
+  { id: 4, name: "Helen, a Diva", university: "UFC", degree: "Design de Moda" }
+];
+
+export { professors };
